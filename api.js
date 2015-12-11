38a293c1e35a6bcde5df64806036facf7734ec78
Matt Karl <matt@mattkarl.com>
2015-12-11 11:50:30 -0500

    Auto-generated commit

--- api.js
+++ api.js
@@ -2,16 +2,22 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "springroll.Container",
+        "springroll.ContainerPlugin",
         "springroll.Features",
         "springroll.SavedDataHandler"
     ],
     "modules": [
-        "Container"
+        "Container",
+        "Core"
     ],
     "allModules": [
         {
             "displayName": "Container",
             "name": "Container"
+        },
+        {
+            "displayName": "Core",
+            "name": "Core"
         }
     ]
 } };
