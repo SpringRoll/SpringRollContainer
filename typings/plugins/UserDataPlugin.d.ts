b34bb68ec76515f3fd060a85ca9dc24719e5ae2a
Andrew Smith <andrew.smith@redspace.com>
2018-10-12 15:42:50 -0300

    added missing typing for UserDataPlugin

--- typings/plugins/UserDataPlugin.d.ts
+++ typings/plugins/UserDataPlugin.d.ts
@@ -3,6 +3,7 @@ import { BasePlugin } from "..";
 type DataFormat = {data: { name:any, value: any, type:string }, type: string};
 
 export class UserDataPlugin extends BasePlugin {
+  constructor();
   open(): void;
   onUserDataRemove($event: DataFormat): void;
   onUserDataRead($event: DataFormat): void;
