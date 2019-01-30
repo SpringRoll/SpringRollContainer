SpringRoll/SpringRollContainer

commit 86a53b48c6553652e592485c9c410013a75184f4

Added error checking for missing elements

src/container/plugins/CaptionsPlugin.js
@@ -2,7 +2,8 @@
  * @module Container
  * @namespace springroll
  */
-(function() {
+(function()
+{
 	var SavedData = include('springroll.SavedData');
 
 	/**
@@ -44,7 +45,8 @@
 	 */
 	var CAPTIONS_MUTED = 'captionsMuted';
 
-	plugin.setup = function() {
+	plugin.setup = function()
+	{
 		/**
 		 * The collection of captions styles
 		 * @property {string} _captionsStyles
@@ -53,7 +55,8 @@
 		this._captionsStyles = Object.merge(
 			{},
 			DEFAULT_CAPTIONS_STYLES,
-			SavedData.read(CAPTIONS_STYLES) || {}
+			SavedData.read(CAPTIONS_STYLES) ||
+			{}
 		);
 
 		/**
@@ -61,9 +64,18 @@
 		 * @property {HTMLElement} captionsButton
 		 */
 		this.captionsButton = document.querySelector(this.options.captionsButton);
+
+		if (null === this.captionsButton)
+		{
+			throw new Error(
+				'No element found with the provided selector for captions button'
+			);
+		}
+
 		this.captionsButton.addEventListener(
 			'click',
-			function() {
+			function()
+			{
 				this.captionsMuted = !this.captionsMuted;
 			}.bind(this)
 		);
@@ -72,12 +84,15 @@
 		 * @property {boolean} captionsMuted
 		 * @default true
 		 */
-		Object.defineProperty(this, CAPTIONS_MUTED, {
-			set: function(muted) {
+		Object.defineProperty(this, CAPTIONS_MUTED,
+		{
+			set: function(muted)
+			{
 				this._captionsMuted = muted;
 				this._setMuteProp(CAPTIONS_MUTED, this.captionsButton, muted);
 			},
-			get: function() {
+			get: function()
+			{
 				return this._captionsMuted;
 			}
 		});
@@ -95,17 +110,23 @@
 		 * @param {string} [styles.align='top'] The align style default is top of the window
 		 * @param {string} [value] If setting styles parameter as a string, this is the value of the property.
 		 */
-		this.setCaptionsStyles = function(styles, value) {
-			if (typeof styles === 'object') {
-				Object.merge(this._captionsStyles, styles || {});
-			} else if (typeof styles === 'string') {
+		this.setCaptionsStyles = function(styles, value)
+		{
+			if (typeof styles === 'object')
+			{
+				Object.merge(this._captionsStyles, styles ||
+				{});
+			}
+			else if (typeof styles === 'string')
+			{
 				this._captionsStyles[styles] = value;
 			}
 
 			styles = this._captionsStyles;
 
 			// Do some validation on the style settings
-			if (DEBUG) {
+			if (DEBUG)
+			{
 				var colorReg = /^(black|white|red|yellow|pink|blue)(-semi)?$/;
 				var backgroundReg = /^none|((black|white|red|yellow|pink|blue)(-semi)?)$/;
 				var sizeReg = /^(xs|sm|md|lg|xl)$/;
@@ -113,31 +134,38 @@
 				var fontReg = /^(georgia|palatino|times|arial|arial-black|comic-sans|impact|lucida|tahoma|trebuchet|verdana|courier|console)$/;
 				var alignReg = /^(top|bottom)$/;
 
-				if (!styles.color || !colorReg.test(styles.color)) {
+				if (!styles.color || !colorReg.test(styles.color))
+				{
 					throw 'Setting captions color style is invalid value : ' +
 						styles.color;
 				}
-				if (!styles.background || !backgroundReg.test(styles.background)) {
+				if (!styles.background || !backgroundReg.test(styles.background))
+				{
 					throw 'Setting captions background style is invalid value : ' +
 						styles.background;
 				}
-				if (!styles.size || !sizeReg.test(styles.size)) {
+				if (!styles.size || !sizeReg.test(styles.size))
+				{
 					throw 'Setting captions size style is invalid value : ' + styles.size;
 				}
-				if (!styles.edge || !edgeReg.test(styles.edge)) {
+				if (!styles.edge || !edgeReg.test(styles.edge))
+				{
 					throw 'Setting captions edge style is invalid value : ' + styles.edge;
 				}
-				if (!styles.font || !fontReg.test(styles.font)) {
+				if (!styles.font || !fontReg.test(styles.font))
+				{
 					throw 'Setting captions font style is invalid value : ' + styles.font;
 				}
-				if (!styles.align || !alignReg.test(styles.align)) {
+				if (!styles.align || !alignReg.test(styles.align))
+				{
 					throw 'Setting captions align style is invalid value : ' +
 						styles.align;
 				}
 			}
 
 			SavedData.write(CAPTIONS_STYLES, styles);
-			if (this.client) {
+			if (this.client)
+			{
 				this.client.send(CAPTIONS_STYLES, styles);
 			}
 		};
@@ -148,7 +176,8 @@
 		 * @param {string} [prop] The optional property, values are "size", "edge", "font", "background", "color"
 		 * @return {object} The collection of styles, see setCaptionsStyles for more info.
 		 */
-		this.getCaptionsStyles = function(prop) {
+		this.getCaptionsStyles = function(prop)
+		{
 			var styles = this._captionsStyles;
 			return prop ? styles[prop] : styles;
 		};
@@ -157,34 +186,41 @@
 		 * Reset the captions styles
 		 * @method clearCaptionsStyles
 		 */
-		this.clearCaptionsStyles = function() {
-			this._captionsStyles = Object.merge({}, DEFAULT_CAPTIONS_STYLES);
+		this.clearCaptionsStyles = function()
+		{
+			this._captionsStyles = Object.merge(
+			{}, DEFAULT_CAPTIONS_STYLES);
 			this.setCaptionsStyles();
 		};
 
 		// Handle the features request
-		this.on('features', function(features) {
+		this.on('features', function(features)
+		{
 			this.captionsButton.style.display = 'none';
 			if (features.captions) this.captionsButton.style.display = 'inline-block';
 		});
 
 		//Set the defaults if we have none for the controls
-		if (SavedData.read(CAPTIONS_MUTED) === null) {
+		if (SavedData.read(CAPTIONS_MUTED) === null)
+		{
 			this.captionsMuted = true;
 		}
 	};
 
-	plugin.opened = function() {
+	plugin.opened = function()
+	{
 		this.captionsButton.classList.remove('disabled');
 		this.captionsMuted = !!SavedData.read(CAPTIONS_MUTED);
 		this.setCaptionsStyles(SavedData.read(CAPTIONS_STYLES));
 	};
 
-	plugin.close = function() {
+	plugin.close = function()
+	{
 		this._disableButton(this.captionsButton);
 	};
 
-	plugin.teardown = function() {
+	plugin.teardown = function()
+	{
 		this.captionsButton.off('click');
 		delete this.captionsButton;
 		delete this._captionsStyles;

dist/container.js
@@ -1336,6 +1336,11 @@
 		 */
 		this.main = document.querySelector(iframeSelector);
 
+		if (null === this.main)
+		{
+			throw new Error('No iframe was found with the provided selector');
+		}
+
 		/**
 		 * The DOM object for the iframe
 		 * @property {Element} dom
@@ -1945,6 +1950,14 @@
 		 * @property {HTMLElement} captionsButton
 		 */
 		this.captionsButton = document.querySelector(this.options.captionsButton);
+
+		if (null === this.captionsButton)
+		{
+			throw new Error(
+				'No element found with the provided selector for captions button'
+			);
+		}
+
 		this.captionsButton.addEventListener(
 			'click',
 			function()
@@ -2000,41 +2013,36 @@
 			// Do some validation on the style settings
 			if (true)
 			{
-				if (!styles.color ||
-					!/^(black|white|red|yellow|pink|blue)(-semi)?$/.test(styles.color)
-				)
+				var colorReg = /^(black|white|red|yellow|pink|blue)(-semi)?$/;
+				var backgroundReg = /^none|((black|white|red|yellow|pink|blue)(-semi)?)$/;
+				var sizeReg = /^(xs|sm|md|lg|xl)$/;
+				var edgeReg = /^(raise|depress|uniform|drop|none)$/;
+				var fontReg = /^(georgia|palatino|times|arial|arial-black|comic-sans|impact|lucida|tahoma|trebuchet|verdana|courier|console)$/;
+				var alignReg = /^(top|bottom)$/;
+
+				if (!styles.color || !colorReg.test(styles.color))
 				{
 					throw 'Setting captions color style is invalid value : ' +
 						styles.color;
 				}
-				if (!styles.background ||
-					!/^none|((black|white|red|yellow|pink|blue)(-semi)?)$/.test(
-						styles.background
-					)
-				)
+				if (!styles.background || !backgroundReg.test(styles.background))
 				{
 					throw 'Setting captions background style is invalid value : ' +
 						styles.background;
 				}
-				if (!styles.size || !/^(xs|sm|md|lg|xl)$/.test(styles.size))
+				if (!styles.size || !sizeReg.test(styles.size))
 				{
 					throw 'Setting captions size style is invalid value : ' + styles.size;
 				}
-				if (!styles.edge ||
-					!/^(raise|depress|uniform|drop|none)$/.test(styles.edge)
-				)
+				if (!styles.edge || !edgeReg.test(styles.edge))
 				{
 					throw 'Setting captions edge style is invalid value : ' + styles.edge;
 				}
-				if (!styles.font ||
-					!/^(georgia|palatino|times|arial|arial-black|comic-sans|impact|lucida|tahoma|trebuchet|verdana|courier|console)$/.test(
-						styles.font
-					)
-				)
+				if (!styles.font || !fontReg.test(styles.font))
 				{
 					throw 'Setting captions font style is invalid value : ' + styles.font;
 				}
-				if (!styles.align || !/^(top|bottom)$/.test(styles.align))
+				if (!styles.align || !alignReg.test(styles.align))
 				{
 					throw 'Setting captions align style is invalid value : ' +
 						styles.align;
@@ -2287,6 +2295,12 @@
 		// has been sent
 		var self = this;
 		var pauseFocus = document.querySelector(this.options.pauseFocusSelector);
+
+		if (null === pauseFocus)
+		{
+			throw new Error('No element found with the provided for pauseFocus');
+		}
+
 		pauseFocus.addEventListener('focus', function()
 		{
 			self._isManualPause = self.paused = true;
@@ -2428,6 +2442,14 @@
 		 * @property {HTMLElement} helpButton
 		 */
 		this.helpButton = document.querySelector(this.options.helpButton);
+
+		if (null === this.helpButton)
+		{
+			throw new Error(
+				'No element found with the provided selector for help button'
+			);
+		}
+
 		this.helpButton.addEventListener(
 			'click',
 			function()
@@ -2538,8 +2560,14 @@
 		 * Reference to the pause application button
 		 * @property {HTMLElement} pauseButton
 		 */
-		console.log('you');
 		this.pauseButton = document.querySelectorAll(this.options.pauseButton);
+
+		if (1 > this.pauseButton.length)
+		{
+			throw new Error(
+				'No element/elements found with provided selector(s) for pause button(s)'
+			);
+		}
 
 		this.pauseButton.forEach(
 			function(element)
@@ -2818,12 +2846,26 @@
 		this.soundButton = document.querySelector(this.options.soundButton);
 		this.soundButton.addEventListener('click', onSoundToggle.bind(this));
 
+		if (null === this.soundButton)
+		{
+			throw new Error(
+				'No element found with provided selector for sound button'
+			);
+		}
+
 		/**
 		 * Reference to the music mute button
 		 * @property {HTMLElement} musicButton
 		 */
 		this.musicButton = document.querySelector(this.options.musicButton);
 		this.musicButton.addEventListener('click', onMusicToggle.bind(this));
+
+		if (null === this.musicButton)
+		{
+			throw new Error(
+				'No element found with provided selector for music button'
+			);
+		}
 
 		/**
 		 * Reference to the sound effects mute button
@@ -2832,12 +2874,24 @@
 		this.sfxButton = document.querySelector(this.options.sfxButton);
 		this.sfxButton.addEventListener('click', onSFXToggle.bind(this));
 
+		if (null === this.sfxButton)
+		{
+			throw new Error('No element found with provided selector for sfx button');
+		}
+
 		/**
 		 * Reference to the voice-over mute button
 		 * @property {HTMLElement} voButton
 		 */
 		this.voButton = document.querySelector(this.options.voButton);
 		this.voButton.addEventListener('click', onVOToggle.bind(this));
+
+		if (null === this.voButton)
+		{
+			throw new Error(
+				'No element found with provided selector for voice-over button'
+			);
+		}
 
 		/**
 		 * Check for when all mutes are muted or unmuted

src/container/Container.js
@@ -50,6 +50,11 @@
 		 */
 		this.main = document.querySelector(iframeSelector);
 
+		if (null === this.main)
+		{
+			throw new Error('No iframe was found with the provided selector');
+		}
+
 		/**
 		 * The DOM object for the iframe
 		 * @property {Element} dom

src/container/plugins/FocusPlugin.js
@@ -137,6 +137,12 @@
 		// has been sent
 		var self = this;
 		var pauseFocus = document.querySelector(this.options.pauseFocusSelector);
+
+		if (null === pauseFocus)
+		{
+			throw new Error('No element found with the provided for pauseFocus');
+		}
+
 		pauseFocus.addEventListener('focus', function()
 		{
 			self._isManualPause = self.paused = true;

src/container/plugins/HelpPlugin.js
@@ -16,6 +16,14 @@
 		 * @property {HTMLElement} helpButton
 		 */
 		this.helpButton = document.querySelector(this.options.helpButton);
+
+		if (null === this.helpButton)
+		{
+			throw new Error(
+				'No element found with the provided selector for help button'
+			);
+		}
+
 		this.helpButton.addEventListener(
 			'click',
 			function()

src/container/plugins/PausePlugin.js
@@ -2,21 +2,31 @@
  * @module Container
  * @namespace springroll
  */
-(function() {
+(function()
+{
 	/**
 	 * @class Container
 	 */
 	var plugin = new springroll.ContainerPlugin(80);
 
-	plugin.setup = function() {
+	plugin.setup = function()
+	{
 		/**
 		 * Reference to the pause application button
 		 * @property {HTMLElement} pauseButton
 		 */
 		this.pauseButton = document.querySelectorAll(this.options.pauseButton);
 
+		if (1 > this.pauseButton.length)
+		{
+			throw new Error(
+				'No element/elements found with provided selector(s) for pause button(s)'
+			);
+		}
+
 		this.pauseButton.forEach(
-			function(element) {
+			function(element)
+			{
 				element.addEventListener('click', onPauseToggle.bind(this));
 			}.bind(this)
 		);
@@ -50,12 +60,16 @@
 		 * @property {Boolean} paused
 		 * @default false
 		 */
-		Object.defineProperty(this, 'paused', {
-			set: function(paused) {
-				if (!this._disablePause) {
+		Object.defineProperty(this, 'paused',
+		{
+			set: function(paused)
+			{
+				if (!this._disablePause)
+				{
 					this._paused = paused;
 
-					if (this.client) {
+					if (this.client)
+					{
 						this.client.send('pause', paused);
 					}
 					/**
@@ -76,7 +90,8 @@
 
 					// Set the pause button state
 					this.pauseButton.forEach(
-						function(element) {
+						function(element)
+						{
 							element.classList.remove('unpaused');
 							element.classList.remove('paused');
 
@@ -85,14 +100,16 @@
 					);
 				}
 			},
-			get: function() {
+			get: function()
+			{
 				return this._paused;
 			}
 		});
 
 		this.on(
 			'features',
-			function(features) {
+			function(features)
+			{
 				if (features.disablePause) this._disablePause = true;
 			}.bind(this)
 		);
@@ -103,14 +120,17 @@
 	 * @method onPauseToggle
 	 * @private
 	 */
-	var onPauseToggle = function() {
+	var onPauseToggle = function()
+	{
 		this.paused = !this.paused;
 		this._isManualPause = this.paused;
 	};
 
-	plugin.opened = function() {
+	plugin.opened = function()
+	{
 		this.pauseButton.forEach(
-			function(element) {
+			function(element)
+			{
 				element.classList.remove('disabled');
 			}.bind(this)
 		);
@@ -119,14 +139,17 @@
 		this.paused = this._paused;
 	};
 
-	plugin.close = function() {
+	plugin.close = function()
+	{
 		this._disableButton(this.pauseButton);
 		this.paused = false;
 	};
 
-	plugin.teardown = function() {
+	plugin.teardown = function()
+	{
 		this.pauseButton.forEach(
-			function(element) {
+			function(element)
+			{
 				element.removeEventListener('click', onPauseToggle.bind(this));
 			}.bind(this)
 		);

src/container/plugins/SoundPlugin.js
@@ -56,12 +56,26 @@
 		this.soundButton = document.querySelector(this.options.soundButton);
 		this.soundButton.addEventListener('click', onSoundToggle.bind(this));
 
+		if (null === this.soundButton)
+		{
+			throw new Error(
+				'No element found with provided selector for sound button'
+			);
+		}
+
 		/**
 		 * Reference to the music mute button
 		 * @property {HTMLElement} musicButton
 		 */
 		this.musicButton = document.querySelector(this.options.musicButton);
 		this.musicButton.addEventListener('click', onMusicToggle.bind(this));
+
+		if (null === this.musicButton)
+		{
+			throw new Error(
+				'No element found with provided selector for music button'
+			);
+		}
 
 		/**
 		 * Reference to the sound effects mute button
@@ -70,12 +84,24 @@
 		this.sfxButton = document.querySelector(this.options.sfxButton);
 		this.sfxButton.addEventListener('click', onSFXToggle.bind(this));
 
+		if (null === this.sfxButton)
+		{
+			throw new Error('No element found with provided selector for sfx button');
+		}
+
 		/**
 		 * Reference to the voice-over mute button
 		 * @property {HTMLElement} voButton
 		 */
 		this.voButton = document.querySelector(this.options.voButton);
 		this.voButton.addEventListener('click', onVOToggle.bind(this));
+
+		if (null === this.voButton)
+		{
+			throw new Error(
+				'No element found with provided selector for voice-over button'
+			);
+		}
 
 		/**
 		 * Check for when all mutes are muted or unmuted

commit 2bbc6cae38e55ddc5052587b72711d7525ebc3f9

Handling missing captions button better
There was a bug introduced in 86a53b48c6553652e592485c9c410013a75184f4
that caused a dependency between existence of the captionsButton and the
captionsMuted property. However, there are many cases where there is no
visible captions button but the developer would like to control captions
from another UI interaction/element/mechanism. This should fix that
functionality

src/container/plugins/CaptionsPlugin.js
@@ -65,17 +65,15 @@
 		 */
 		this.captionsButton = document.querySelector(this.options.captionsButton);
 
-		if (null === this.captionsButton)
+		if (null !== this.captionsButton)
 		{
-			return;
+			this.captionsButtonClick = function()
+			{
+				this.captionsMuted = !this.captionsMuted;
+			}.bind(this);
+
+			this.captionsButton.addEventListener('click', this.captionsButtonClick);
 		}
-
-		this.captionsButtonClick = function()
-		{
-			this.captionsMuted = !this.captionsMuted;
-		}.bind(this);
-
-		this.captionsButton.addEventListener('click', this.captionsButtonClick);
 
 		/**
 		 * Set the captions are enabled or not
@@ -194,8 +192,11 @@
 		// Handle the features request
 		this.on('features', function(features)
 		{
-			this.captionsButton.style.display = 'none';
-			if (features.captions) this.captionsButton.style.display = 'inline-block';
+			if (null !== this.captionsButton)
+			{
+				this.captionsButton.style.display = 'none';
+				if (features.captions) this.captionsButton.style.display = 'inline-block';
+			}
 		});
 
 		//Set the defaults if we have none for the controls
@@ -207,35 +208,31 @@
 
 	plugin.opened = function()
 	{
-		if (null === this.captionsButton)
+		if (null !== this.captionsButton)
 		{
-			return;
+			this.captionsButton.classList.remove('disabled');
 		}
 
-		this.captionsButton.classList.remove('disabled');
 		this.captionsMuted = !!SavedData.read(CAPTIONS_MUTED);
 		this.setCaptionsStyles(SavedData.read(CAPTIONS_STYLES));
 	};
 
 	plugin.close = function()
 	{
-		if (null === this.captionsButton)
+		if (null !== this.captionsButton)
 		{
-			return;
+			this._disableButton(this.captionsButton);
 		}
-
-		this._disableButton(this.captionsButton);
 	};
 
 	plugin.teardown = function()
 	{
-		if (null === this.captionsButton)
+		if (null !== this.captionsButton)
 		{
-			return;
+			this.captionsButton.removeEventListener('click', this.captionsButtonClick);
+			delete this.captionsButton;
 		}
 
-		this.captionsButton.removeEventListener('click', this.captionsButtonClick);
-		delete this.captionsButton;
 		delete this._captionsStyles;
 		delete this.getCaptionsStyles;
 		delete this.setCaptionsStyles;