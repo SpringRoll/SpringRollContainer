3d59168fa6e7525a29c344c67fe55f81da3a0d8b
Andrew Smith <andrew.smith@redspace.com>
2019-05-02 15:46:39 -0300

    updated Container.spec tests

--- src/Container.spec.js
+++ src/Container.spec.js
@@ -15,7 +15,7 @@ describe('Container', () => {
     container.onLoadDone();
     expect(container.loading).to.be.false;
     expect(container.loaded).to.be.true;
-    expect(container.main.classList.contains('loading')).to.be.false;
+    expect(container.iFrame.classList.contains('loading')).to.be.false;
   });
 
   it('.initClient()', () => {
@@ -25,19 +25,19 @@ describe('Container', () => {
 
   it('._internalOpen', () => {
     container._internalOpen('test');
-    expect(container.main.src).to.contain('http://localhost:9876/');
+    expect(container.iFrame['src']).to.contain('http://localhost:9876/');
   });
 
   it('.openPath', () => {
     container.openPath('test');
-    expect(container.main.src).to.contain('http://localhost:9876/');
+    expect(container.iFrame['src']).to.contain('http://localhost:9876/');
   });
 
   it('.reset(), .onEndGame(), _onCloseFailed()', () => {
     container.reset();
     expect(container.loaded).to.be.false;
     expect(container.loading).to.be.false;
-    expect(container.main.src).to.contain('http://localhost:9876/');
+    expect(container.iFrame['src']).to.contain('http://localhost:9876/');
   });
 
   it('.openRemote()', () => {
@@ -46,9 +46,8 @@ describe('Container', () => {
 
   it('.destroy()', () => {
     container.destroy();
-    expect(container.main).to.be.null;
+    expect(container.iFrame).to.be.null;
     expect(container.options).to.be.null;
-    expect(container.dom).to.be.null;
   });
 
   it('Container.version() - get', () => {
