02973913fd2a81c8ee42d611dd9185de6464db2b
Sean Ryan <902seanryan@gmail.com>
2020-02-10 16:53:07 -0400

    Added in multiple difficulty options for difficulty plugin

--- src/plugins/DifficultyPlugin.js
+++ src/plugins/DifficultyPlugin.js
@@ -14,30 +14,196 @@ export class DifficultyPlugin extends BasePlugin {
    * @param {number} [params.defaultDifficulty=0.5]
    * @memberof DifficultyPlugin
    */
-  constructor({ difficultySlider, defaultDifficulty = 0.5 }) {
+  constructor({
+    hitAreaScaleSlider,
+    defaultHitAreaScale = 0.5,
+    dragThresholdScaleSlider,
+    defaultDragThresholdScale = 0.5,
+    healthSlider,
+    defaultHealth = 0.5,
+    objectCountSlider,
+    defaultObjectCount = 0.5,
+    completionPercentageSlider,
+    defaultCompletionPercentage = 0.5,
+    speedScaleSlider,
+    defaultSpeedScale = 0.5,
+    timersScaleSlider,
+    defaultTimersScale = 0.5,
+    inputCountSlider,
+    defaultInputCount = 0.5,
+  } = {}) {
     super('Difficulty-Plugin');
 
-    this.difficulty = defaultDifficulty;
+    this.hitAreaScale = defaultHitAreaScale;
+    this.dragThresholdScale = defaultDragThresholdScale;
+    this.health = defaultHealth;
+    this.objectCount = defaultObjectCount;
+    this.completionPercentage = defaultCompletionPercentage;
+    this.speedScale = defaultSpeedScale;
+    this.timersScale = defaultTimersScale;
+    this.inputCount = defaultInputCount;
 
-    this.difficultySlider = new Slider({
-      slider: difficultySlider,
-      control: DifficultyPlugin.difficultyKey,
-      defaultValue: this.difficulty
+    //Hit Area Scale
+    this.hitAreaScaleSlider = new Slider({
+      slider: hitAreaScaleSlider,
+      control: DifficultyPlugin.hitAreaKey,
+      defaultValue: this.hitAreaScale
     });
 
-    this.difficultySlider.enableSliderEvents(
-      this.onDifficultyChange.bind(this)
+    this.hitAreaScaleSlider.enableSliderEvents(
+      this.onHitAreaScaleChange.bind(this)
     );
+
+    //Drag Threshold Scale
+    this.dragThresholdScaleSlider = new Slider({
+      slider: dragThresholdScaleSlider,
+      control: DifficultyPlugin.dragThresholdScaleKey,
+      defaultValue: this.dragThresholdScale
+    });
+
+    this.dragThresholdScaleSlider.enableSliderEvents(
+      this.onDragThresholdScaleChange.bind(this)
+    );
+
+    //Health
+    this.healthSlider = new Slider({
+      slider: healthSlider,
+      control: DifficultyPlugin.healthKey,
+      defaultValue: this.health
+    });
+
+    this.healthSlider.enableSliderEvents(
+      this.onHealthChange.bind(this)
+    );
+
+    //Object Count
+    this.objectCountSlider = new Slider({
+      slider: objectCountSlider,
+      control: DifficultyPlugin.objectCountKey,
+      defaultValue: this.objectCount
+    });
+
+    this.objectCountSlider.enableSliderEvents(
+      this.onObjectCountChange.bind(this)
+    );
+
+    //Completion Percentage
+    this.completionPercentageSlider = new Slider({
+      slider: completionPercentageSlider,
+      control: DifficultyPlugin.completionPercentageKey,
+      defaultValue: this.completionPercentage
+    });
+
+    this.completionPercentageSlider.enableSliderEvents(
+      this.onCompletionPercentageChange.bind(this)
+    );
+
+    //Speed Scale
+    this.speedScaleSlider = new Slider({
+      slider: speedScaleSlider,
+      control: DifficultyPlugin.speedScaleKey,
+      defaultValue: this.speedScale
+    });
+
+    this.speedScaleSlider.enableSliderEvents(
+      this.onSpeedScaleChange.bind(this)
+    );
+
+    //Timer Scale
+    this.timersScaleSlider = new Slider({
+      slider: timersScaleSlider,
+      control: DifficultyPlugin.timersScaleKey,
+      defaultValue: this.timersScale
+    });
+
+    this.timersScaleSlider.enableSliderEvents(
+      this.onTimersScaleChange.bind(this)
+    );
+
+    //Input Count
+    this.inputCountSlider = new Slider({
+      slider: inputCountSlider,
+      control: DifficultyPlugin.inputCountKey,
+      defaultValue: this.inputCount
+    });
+
+    this.inputCountSlider.enableSliderEvents(
+      this.onInputCountChange.bind(this)
+    );
+
   }
 
   /**
    * @memberof ControlsPlugin
    */
-  onDifficultyChange() {
-    this.difficulty = this.difficultySlider.sliderRange(
-      Number(this.difficultySlider.slider.value)
+  onHitAreaScaleChange() {
+    this.hitAreaScale = this.hitAreaScaleSlider.sliderRange(
+      Number(this.hitAreaScaleSlider.slider.value)
     );
-    this.sendProperty(DifficultyPlugin.difficultyKey, this.difficulty);
+    this.sendProperty(DifficultyPlugin.hitAreaScaleKey, this.hitAreaScale);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onDragThresholdScaleChange() {
+    this.dragThresholdScale = this.dragThresholdScaleSlider.sliderRange(
+      Number(this.dragThresholdScaleSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.dragThresholdScaleKey, this.dragThresholdScale);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onHealthChange() {
+    this.health = this.healthSlider.sliderRange(
+      Number(this.healthSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.healthKey, this.health);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onObjectCountChange() {
+    this.objectCount = this.objectCountSlider.sliderRange(
+      Number(this.objectCountSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.objectCountKey, this.objectCount);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onCompletionPercentageChange() {
+    this.completionPercentage = this.completionPercentageSlider.sliderRange(
+      Number(this.completionPercentageSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.completionPercentageKey, this.completionPercentage);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onSpeedScaleChange() {
+    this.speedScale = this.speedScaleSlider.sliderRange(
+      Number(this.speedScaleSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.speedScaleKey, this.speedScale);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onTimersScaleChange() {
+    this.timersScale = this.timersScaleSlider.sliderRange(
+      Number(this.timersScaleSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.timersScaleKey, this.timersScale);
+  }
+  /**
+   * @memberof ControlsPlugin
+   */
+  onInputCountChange() {
+    this.inputCount = this.inputCountSlider.sliderRange(
+      Number(this.inputCountSlider.slider.value)
+    );
+    this.sendProperty(DifficultyPlugin.inputCountKey, this.inputCount);
   }
 
   /**
@@ -65,7 +231,63 @@ export class DifficultyPlugin extends BasePlugin {
    * @static
    * @memberof ControlsPlugin
    */
-  static get difficultyKey() {
-    return 'difficulty';
+  static get hitAreaScaleKey() {
+    return 'hitAreaScale';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get dragThresholdScaleKey() {
+    return 'dragThresholdScale';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get healthKey() {
+    return 'health';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get objectCountKey() {
+    return 'objectCount';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get completionPercentageKey() {
+    return 'completionPercentage';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get speedScaleKey() {
+    return 'speedScale';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get timersScaleKey() {
+    return 'timersScale';
+  }
+  /**
+   * @readonly
+   * @static
+   * @memberof ControlsPlugin
+   */
+  static get inputCountKey() {
+    return 'inputCount';
   }
 }
--- src/plugins/DifficultyPlugin.spec.js
+++ src/plugins/DifficultyPlugin.spec.js
@@ -9,14 +9,27 @@ const initEvent = eventName => {
 
 describe('DifficultyPlugin', () => {
   let dp;
+  const options = {
+    hitAreaScaleSlider: 'hass',
+    dragThresholdScaleSlider: 'dtss',
+    healthSlider: 'hs',
+    objectCountSlider: 'ocs',
+    completionPercentageSlider: 'cps',
+    speedScaleSlider: 'sss',
+    timersScaleSlider: 'tss',
+    inputCountSlider: 'ics'
+  };
 
   before(() => {
     document.body.innerHTML = '';
-    const slider = document.createElement('input');
-    slider.type = 'range';
-    slider.id = 'ss';
-    document.body.appendChild(slider);
-    dp = new DifficultyPlugin({ difficultySlider: '#ss' });
+    Object.keys(options).forEach(key => {
+      const slider = document.createElement('input');
+      slider.type = 'range';
+      slider.id = options[key];
+      options[key] = `#${options[key]}`;
+      document.body.appendChild(slider);
+    });
+    dp = new DifficultyPlugin(options);
     dp.preload({ client: new Bellhop() });
   });
 
@@ -24,36 +37,216 @@ describe('DifficultyPlugin', () => {
     const iframe = document.createElement('iframe');
     iframe.id = 'difficulty-plugin-iframe';
     document.body.appendChild(iframe);
-    new Container({
-      iframeSelector: '#difficulty-plugin-iframe'
-    }).client.trigger('features');
+    new Container({ iframeSelector: '#difficulty-plugin-iframe' }).client.trigger(
+      'features'
+    );
   });
 
-  it('.onControldifficultyChange()', () => {
-    dp.difficultySlider.value = 1;
-    dp.difficultySlider.dispatchEvent(initEvent('change'));
+  it('.onHitAreaScaleChange()', () => {
+    dp.hitAreaScaleSlider.value = 1;
+    dp.hitAreaScaleSlider.dispatchEvent(initEvent('change'));
 
-    expect(dp.difficultySlider.value).to.equal('1');
-    expect(dp.difficulty).to.equal(1);
+    expect(dp.hitAreaScaleSlider.value).to.equal('1');
+    expect(dp.hitAreaScale).to.equal(1);
 
-    dp.difficultySlider.value = 0;
-    dp.difficultySlider.dispatchEvent(initEvent('change'));
+    dp.hitAreaScaleSlider.value = 0;
+    dp.hitAreaScaleSlider.dispatchEvent(initEvent('change'));
 
-    expect(dp.difficultySlider.value).to.equal('0');
-    expect(dp.difficulty).to.equal(0);
+    expect(dp.hitAreaScaleSlider.value).to.equal('0');
+    expect(dp.hitAreaScale).to.equal(0);
 
+    dp.hitAreaScaleSlider.value = 1.1;
+    dp.hitAreaScaleSlider.dispatchEvent(initEvent('change'));
 
-    dp.difficultySlider.value = 1.1;
-    dp.difficultySlider.dispatchEvent(initEvent('change'));
+    expect(dp.hitAreaScaleSlider.value).to.equal('1');
+    expect(dp.hitAreaScale).to.equal(1);
 
-    expect(dp.difficultySlider.value).to.equal('1');
-    expect(dp.difficulty).to.equal(1);
+    dp.hitAreaScaleSlider.value = -1;
+    dp.hitAreaScaleSlider.dispatchEvent(initEvent('change'));
 
+    expect(dp.hitAreaScaleSlider.value).to.equal('0');
+    expect(dp.hitAreaScale).to.equal(0);
+  });
 
-    dp.difficultySlider.value = -1;
-    dp.difficultySlider.dispatchEvent(initEvent('change'));
+  it('.onDragThresholdScaleChange()', () => {
+    dp.dragThresholdScaleSlider.value = 1;
+    dp.dragThresholdScaleSlider.dispatchEvent(initEvent('change'));
 
-    expect(dp.difficultySlider.value).to.equal('0');
-    expect(dp.difficulty).to.equal(0);
+    expect(dp.dragThresholdScaleSlider.value).to.equal('1');
+    expect(dp.dragThresholdScale).to.equal(1);
+
+    dp.dragThresholdScaleSlider.value = 0;
+    dp.dragThresholdScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.dragThresholdScaleSlider.value).to.equal('0');
+    expect(dp.dragThresholdScale).to.equal(0);
+
+    dp.dragThresholdScaleSlider.value = 1.1;
+    dp.dragThresholdScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.dragThresholdScaleSlider.value).to.equal('1');
+    expect(dp.dragThresholdScale).to.equal(1);
+
+    dp.dragThresholdScaleSlider.value = -1;
+    dp.dragThresholdScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.dragThresholdScaleSlider.value).to.equal('0');
+    expect(dp.dragThresholdScale).to.equal(0);
+  });
+
+  it('.onHealthChange()', () => {
+    dp.healthSlider.value = 1;
+    dp.healthSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.healthSlider.value).to.equal('1');
+    expect(dp.health).to.equal(1);
+
+    dp.healthSlider.value = 0;
+    dp.healthSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.healthSlider.value).to.equal('0');
+    expect(dp.health).to.equal(0);
+
+    dp.healthSlider.value = 1.1;
+    dp.healthSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.healthSlider.value).to.equal('1');
+    expect(dp.health).to.equal(1);
+
+    dp.healthSlider.value = -1;
+    dp.healthSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.healthSlider.value).to.equal('0');
+    expect(dp.health).to.equal(0);
+  });
+
+  it('.onObjectCountChange()', () => {
+    dp.objectCountSlider.value = 1;
+    dp.objectCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.objectCountSlider.value).to.equal('1');
+    expect(dp.objectCount).to.equal(1);
+
+    dp.objectCountSlider.value = 0;
+    dp.objectCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.objectCountSlider.value).to.equal('0');
+    expect(dp.objectCount).to.equal(0);
+
+    dp.objectCountSlider.value = 1.1;
+    dp.objectCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.objectCountSlider.value).to.equal('1');
+    expect(dp.objectCount).to.equal(1);
+
+    dp.objectCountSlider.value = -1;
+    dp.objectCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.objectCountSlider.value).to.equal('0');
+    expect(dp.objectCount).to.equal(0);
+  });
+
+  it('.onCompletionPercentageChange()', () => {
+    dp.completionPercentageSlider.value = 1;
+    dp.completionPercentageSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.completionPercentageSlider.value).to.equal('1');
+    expect(dp.completionPercentage).to.equal(1);
+
+    dp.completionPercentageSlider.value = 0;
+    dp.completionPercentageSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.completionPercentageSlider.value).to.equal('0');
+    expect(dp.completionPercentage).to.equal(0);
+
+    dp.completionPercentageSlider.value = 1.1;
+    dp.completionPercentageSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.completionPercentageSlider.value).to.equal('1');
+    expect(dp.completionPercentage).to.equal(1);
+
+    dp.completionPercentageSlider.value = -1;
+    dp.completionPercentageSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.completionPercentageSlider.value).to.equal('0');
+    expect(dp.completionPercentage).to.equal(0);
+  });
+
+  it('.onSpeedScaleChange()', () => {
+    dp.speedScaleSlider.value = 1;
+    dp.speedScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.speedScaleSlider.value).to.equal('1');
+    expect(dp.speedScale).to.equal(1);
+
+    dp.speedScaleSlider.value = 0;
+    dp.speedScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.speedScaleSlider.value).to.equal('0');
+    expect(dp.speedScale).to.equal(0);
+
+    dp.speedScaleSlider.value = 1.1;
+    dp.speedScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.speedScaleSlider.value).to.equal('1');
+    expect(dp.speedScale).to.equal(1);
+
+    dp.speedScaleSlider.value = -1;
+    dp.speedScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.speedScaleSlider.value).to.equal('0');
+    expect(dp.speedScale).to.equal(0);
+  });
+
+  it('.onTimersScaleChange()', () => {
+    dp.timersScaleSlider.value = 1;
+    dp.timersScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.timersScaleSlider.value).to.equal('1');
+    expect(dp.timersScale).to.equal(1);
+
+    dp.timersScaleSlider.value = 0;
+    dp.timersScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.timersScaleSlider.value).to.equal('0');
+    expect(dp.timersScale).to.equal(0);
+
+    dp.timersScaleSlider.value = 1.1;
+    dp.timersScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.timersScaleSlider.value).to.equal('1');
+    expect(dp.timersScale).to.equal(1);
+
+    dp.timersScaleSlider.value = -1;
+    dp.timersScaleSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.timersScaleSlider.value).to.equal('0');
+    expect(dp.timersScale).to.equal(0);
+  });
+
+  it('.onInputCountChange()', () => {
+    dp.inputCountSlider.value = 1;
+    dp.inputCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.inputCountSlider.value).to.equal('1');
+    expect(dp.inputCount).to.equal(1);
+
+    dp.inputCountSlider.value = 0;
+    dp.inputCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.inputCountSlider.value).to.equal('0');
+    expect(dp.inputCount).to.equal(0);
+
+    dp.inputCountSlider.value = 1.1;
+    dp.inputCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.inputCountSlider.value).to.equal('1');
+    expect(dp.inputCount).to.equal(1);
+
+    dp.inputCountSlider.value = -1;
+    dp.inputCountSlider.dispatchEvent(initEvent('change'));
+
+    expect(dp.inputCountSlider.value).to.equal('0');
+    expect(dp.inputCount).to.equal(0);
   });
 });
