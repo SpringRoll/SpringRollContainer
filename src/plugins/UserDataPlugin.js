e81f30055890ec1b328375c540b5e7c8d1509b9b
Bailey Brightman <BaileyBrightman@gmail.com>
2020-12-22 14:06:02 -0400

    Removed changes to userDataClass moved to new branch

--- src/plugins/UserDataPlugin.js
+++ src/plugins/UserDataPlugin.js
@@ -16,11 +16,6 @@ export class UserDataPlugin extends BasePlugin {
     this.onUserDataRemove = this.onUserDataRemove.bind(this);
     this.onUserDataRead = this.onUserDataRead.bind(this);
     this.onUserDataWrite = this.onUserDataWrite.bind(this);
-
-    this.onAdd = this. onAdd.bind(this);
-    this.onOpenDb = this.onOpenDb.bind(this);
-
-    this.db = null;
   }
 
   /**
@@ -32,9 +27,6 @@ export class UserDataPlugin extends BasePlugin {
     this.client.on('userDataRemove', this.onUserDataRemove);
     this.client.on('userDataRead', this.onUserDataRead);
     this.client.on('userDataWrite', this.onUserDataWrite);
-
-    this.client.on('addNote', this.onAddNote);
-    this.client.on('penDb', this.onOpenDb);
   }
 
   /**
@@ -64,135 +56,5 @@ export class UserDataPlugin extends BasePlugin {
    */
   onUserDataWrite({ type, data: { name, value } }) {
     SavedDataHandler.write(name, value, () => this.client.send(type));
-  }
-
-  /**
-   *  
-   */
-  onAdd(storeName, record) {
-    const tx = this.db.transaction(storeName, 'readwrite');
-    tx.onerror = e => alert( ` Error! ${e.target.error}  `);
-    const store = tx.objectStore();
-    store.add(record);
-  }
-
-  /**
-   * Closes the connection to the database
-   */
-  closeDb() {
-    this.db.close();
-  }
-
-  /**
-   * 
-   * @param {*} storeName 
-   * @param {*} note 
-   */
-  onDelete(storeName, key) {
-    const tx = this.db.transaction(storeName, 'readwrite');
-    tx.onerror = e => alert( ` Error! ${e.target.error}  `);
-    const store = tx.objectStore();
-    store.delete(key);
-  }
-
-  /**
-   * 
-   * @param {string} dbName The name of your IndexedDB database
-   * @param {string} dbVersion The version number of the database
-   * @param {string} additions Any 
-   */
-  onOpenDb( dbName, dbVersion = null, additions = {}) {
-    const request = dbVersion ? indexedDB.open(dbName,dbVersion): indexedDB.open(dbName);
-
-    request.onsuccess = e => {
-      this.db = e.target.result;
-      console.log(this.db);
-    };
-
-    //on upgrade needed
-    request.onupgradeneeded = e => {
-      this.db = e.target.result;
-
-      if (additions.stores) {
-        // console.log(additions.stores);
-        additions.stores.forEach(e => {
-          // console.log(e.keyPath);
-          this.db.createObjectStore(e.storeName, e.options);
-          
-        });
-      }
-      if (additions.indexes) {
-        additions.indexes.forEach(e => {
-          // Open a transaction with the target store returning a store object
-          const store = request.transaction.objectStore(e.storeName);
-          
-          console.log(e);
-          store.createIndex(e.indexName, e.keyPath, e.optionalParameters);
-        });
-      }
-      // const store = this.db.createObjectStore('personal_notes', {keyPath: 'title'});
-
-      // alert(`upgrade is called database name: ${this.db.name} version : ${this.db.version}`);
-    };
-
-
-    request.onerror = e => {
-      console.log(e);
-    };
-
-
-    
-  }
-
-
-  
-
-
-  /**
-   * 
-   * @param {string} storeName 
-   */
-  getStoreCursor(storeName) {
-
-    const tx = this.db.transaction(storeName,'readonly');
-    const pNotes = tx.objectStore(storeName);
-    const request = pNotes.openCursor();
-    request.onsuccess = e => {
-
-      const cursor = e.target.result;
-
-      // const respond = async () {
-        
-      // }
-
-
-
-      if (cursor) {
-        alert(`Title: ${cursor.key} Text: ${cursor.value.text} `);
-
-        // await this.client.send('IDBReadResponce', {key: cursor.key, value: cursor.value.text})
-        //do something with the cursor
-        cursor.continue();
-      }
-    };
-
-  }
-
-  /**
-   * 
-   */
-  getRecord(storeName, key) {
-    const tx = this.db.transaction(storeName, 'readonly');
-    const store = tx.objectStore(storeName);
-
-    store.get(key);
-
-    let result;
-
-    store.onsuccess = (e) => {
-      result = e.result;
-    };
-
-    return result;
   }
 }
--- typings/SavedData.d.ts
+++ typings/SavedData.d.ts
@@ -4,6 +4,12 @@ export class SavedData {
   static remove(name: string): void;
   static write(name: sttring, value: any, tempOnly: boolean): void;
 
+  static onOpenDb(dbName: string, dbVersion: INT | null, additions: JSON): void;
+  static onIDBAdd(storeName: string, record: any): void;
+  static onIDBDelete(storeName: string, key: any): void;
+  static onIDBRead(storeName: string, key: any): void;
+  static closeDb(): void;
+
   static get WEB_STORAGE_SUPPORT(): boolean;
   static get ERASE_COOKIE(): boolean;
   static set ERASE_COOKIE(flag: boolean): void;
