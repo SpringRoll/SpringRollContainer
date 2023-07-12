62b37dbb1f8931472d16c681c9e1545a02475f81
902seanryan <sean.ryan@redspace.com>
2023-07-12 16:36:06 -0300

    re-fix rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,45 +6,44 @@ import { babel } from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 
 
-const base = {
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    json(),
-    eslint(),
-    babel({ sourceMap: true }),
-    terser()
-  ]
-};
+const plugins = [
+  nodeResolve(),
+  commonjs(),
+  json(),
+  eslint(),
+  babel({ sourceMap: true }),
+  terser()
+];
 
-const e2e = Object.assign({}, base, {
-  input: 'e2e/client-source.js',
-  output: {
-    file: 'e2e/client.js',
-    format: 'es'
+export default [
+  {
+    input: 'e2e/client-source.js',
+    output: {
+      file: 'e2e/client.js',
+      format: 'es'
+    },
+    plugins
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'es'
+    },
+    plugins
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/SpringRoll-Container-umd.js',
+      format: 'umd',
+      name: 'springroll',
+      extend: true,
+      sourcemap: true,
+      globals: {
+        'bellhop-iframe': 'Bellhop'
+      }
+    },
+    plugins
   }
-});
-
-const es = Object.assign({}, base, {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'es'
-  }
-});
-
-const umd = Object.assign({}, base, {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/SpringRoll-Container-umd.js',
-    format: 'umd',
-    name: 'springroll',
-    extend: true,
-    sourcemap: true,
-    globals: {
-      'bellhop-iframe': 'Bellhop'
-    }
-  }
-});
-
-module.exports = [umd, es, e2e];
+];
